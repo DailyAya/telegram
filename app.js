e3458db36d5ba7055aaf801bfebb76c5edd2c86c
M. Sherbeeny <sherbeeny@me.com>
2021-09-14 13:22:54 +0200

    push

--- app.js
+++ app.js
@@ -45,50 +45,6 @@ bot.hears('aya', ctx => {
 
 
 
-//method for requesting user's phone number
-bot.hears('phone', (ctx, next) => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Can we get access to your phone number?', requestPhoneKeyboard);
-
-})
-
-//method for requesting user's location
-bot.hears("location", (ctx) => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Can we access your location?', requestLocationKeyboard);
-})
-
-//constructor for providing phone number to the bot
-const requestPhoneKeyboard = {
-    "reply_markup": {
-        "one_time_keyboard": true,
-        "keyboard": [
-            [{
-                text: "My phone number",
-                request_contact: true,
-                one_time_keyboard: true
-            }],
-            ["Cancel"]
-        ]
-    }
-};
-
-//constructor for proving location to the bot
-const requestLocationKeyboard = {
-    "reply_markup": {
-        "one_time_keyboard": true,
-        "keyboard": [
-            [{
-                text: "My location",
-                request_location: true,
-                one_time_keyboard: true
-            }],
-            ["Cancel"]
-        ]
-    }
-
-}
-
 //method to start get the script to pulling updates for telegram 
 bot.launch()
 
