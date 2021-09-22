786b1f7fd2f78a40e9196e33eb2cc69f7e8c51c2
M. Sherbeeny <sherbeeny@me.com>
2021-09-23 01:48:20 +0200

    ya rab

--- app.js
+++ app.js
@@ -316,7 +316,7 @@ function isValidReciter(reciter){
 bot.hears('Pro', ctx =>{
     var files = [
         "https://cdn.islamic.network/quran/audio/128/ar.minsawi/5091.mp3",
-        "https://cdn.islamic.network/quran/audio/128/ar.muhammadjibreel/5281.mp3",
+        "https://cdn.islamic.network/quran/audio/128/ar.muhammadjibeel/5281.mp3",
         "https://cdn.islamic.network/quran/audio/128/ar.muhammadjibreel/130.mp3"
     ]
     audioPicker(files, 0)
