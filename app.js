62a87898cdea5f0cfff9115b5a241424192109aa
Sherbeeny <Sherbeeny@me.com>
2022-01-31 09:55:35 +0200

    ya rab

--- app.js
+++ app.js
@@ -915,7 +915,7 @@ bot.start(ctx => {
             if(ctx.startPayload.length) handleText(ctx)
             else start(ctx.chat.id)
         } else {
-            ctx.answerCbQuery("Only admins can interact with DailyAya. \n\nPress on Sura name to open DailyAya privately.", {show_alert: true})???
+            ctx.answerCbQuery("Only admins can interact with DailyAya. \n\nPress on Sura name to open DailyAya privately.", {show_alert: true})
         }
     })
     .catch(e =>{
