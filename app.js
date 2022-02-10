9434acf7bb990fbe392e83cca4683721e8afd116
Sherbeeny <Sherbeeny@me.com>
2022-02-10 15:15:49 +0200

    ya rab

--- app.js
+++ app.js
@@ -566,7 +566,7 @@ function sendAyaRecitation(ctx, ayaId, reciter){
                         bot.telegram.sendAudio(chatId, recitationUrl, {caption: recitationCaption, parse_mode: 'HTML', disable_notification: true})
                             .then((c) =>{
                                 audioSuccess = true
-                                var {message_id} = ctx || ctx.update.callback_query.message
+                                var message_id = ctx.message_id || ctx.update.callback_query.message.message_id
                                 if (c.message_id != 1 + message_id){ // Refer/Reply to the text if the recitation is not sent right after it
                                     audioSuccess = false
                                     bot.telegram.deleteMessage(chatId, c.message_id)
