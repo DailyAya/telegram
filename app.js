5a7325b5a1b43dbfdb55971e4e45d96bb0157f5d
Sherbeeny <Sherbeeny@me.com>
2022-03-02 05:28:05 +0200

    ya rab

--- app.js
+++ app.js
@@ -842,8 +842,8 @@ function handleText(ctx){
             for (let index = 0; index < normalizedSurasArNames.length; index++) {
                 let regex = new RegExp(
                     `(^${normalizedSurasArNames[index]}$)|(^${
-                    normalizedSurasArNames[index]}( +)(.*))|((.*)( +)${
-                    normalizedSurasArNames[index]}( +)(.*))|((.*)( +)${normalizedSurasArNames[index]}$)`
+                    normalizedSurasArNames[index]}([: \d\-]+)(.*))|((.*)( +)${
+                    normalizedSurasArNames[index]}([: \d\-]+)(.*))|((.*)( +)${normalizedSurasArNames[index]}$)`
                     )
                 
                 if(regex.test(normalizedTxt)){
