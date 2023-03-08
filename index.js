75f362191c3cfd8459958106613bc9b1245c2fbe
M. Sherbeeny <sherbeeny@me.com>
2023-03-08 15:40:51 +0000

    fixed instStateMsg as a function instead of a variable.

--- index.js
+++ index.js
@@ -53,7 +53,12 @@ function log(x, e){
 }
 
 
-
+function instStateMsg(){
+  return `DailyAyaTelegram ${branch} instance ${inst}@${host} (of total ${totalInst}) is active in ${
+    debugging ? 'debugging' : 'normal'} mode of version ${codeVer} until ${instActivetUntil}.\n
+Memory Used: ${Math.floor(process.memoryUsage().rss / (1024 * 1024))} MB\n
+Uptime: ${+(process.uptime()/3600).toFixed(2)} hours`
+}
 
 
 // just for heroku web dyno and to manage sleep and balance between multiple instances
@@ -64,12 +69,7 @@ const port = process.env.PORT ?? 3000
 // main route will respond instStateMsg when requested.
 // we call it every 15 minutes using a google app script to prevent the app from sleeping.
 expressApp.get('/', (req, res) => {
-	var instStateMsg = `DailyAyaTelegram ${branch} instance ${inst}@${host} (of total ${totalInst}) is active in ${
-    debugging ? 'debugging' : 'normal'} mode of version ${codeVer} until ${instActivetUntil}.\n
-Memory Used: ${Math.floor(process.memoryUsage().rss / (1024 * 1024))} MB\n
-Uptime: ${+(process.uptime()/3600).toFixed(2)} hours`
-	
-  res.send(instStateMsg)
+  res.send(instStateMsg())
 })
 expressApp.listen(port, () => {
   log(`Listening on port ${port}`)
@@ -277,7 +277,7 @@ bot.telegram.getMe().then((botInfo) => { // for handling group commands without 
 
 // Inform Dev group about the instance state
 if(telegramToken){
-    bot.telegram.sendMessage(devChatId, instStateMsg)
+    bot.telegram.sendMessage(devChatId, instStateMsg())
         .catch(er => log(`Error while sending instance state: `, er))
 }
 
